5fc52ee36f46ee1c75bac64f77bf42520ce08556
jean-marc <jean-marc@lg21.be>
2016-06-24 14:50:43 +0200

    moved bundle files to root

--- DependencyInjection/ForestExtension.php
+++ DependencyInjection/ForestExtension.php
@@ -22,7 +22,7 @@ class ForestExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('routing.yml');
         $loader->load('services.yml');
     }
