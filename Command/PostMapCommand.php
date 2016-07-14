94aac9a543ae14002888d9a4a8e0ac64b798ab03
jean-marc <jean-marc@lg21.be>
2016-07-14 12:13:22 +0200

    added console command for post map to forest server

---
+++ Command/PostMapCommand.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace ForestAdmin\ForestBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use GuzzleHttp\Exception\ClientException;
+use Symfony\Component\DependencyInjection\Exception\InvalidArgumentException;
+
+
+class PostMapCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName('forest:postmap')
+            ->setDescription('Post the API map to the Forest Server')
+//            ->addArgument(
+//                'env',
+//                InputArgument::OPTIONAL,
+//                'From which environment does come the map that you want to post?'
+//            )
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        try {
+            //$this->getSecretKey();
+            $forest = $this->getContainer()->get('forestadmin.forest');
+            //$name = $input->getArgument('name');
+
+                if($forest->postApimap()) {
+                    $text = 'Success!';
+                } else {
+                    $text = 'Failure!';
+                }
+        } catch(ClientException $exc) {
+            $text = 'Failure: '.$exc->getMessage();
+        } catch(InvalidArgumentException $exc) {
+            $text = "Failure: ".$exc->getMessage()."\n"
+            ."Configure your secret key with the key you received when you registered your app to Forest.";
+        }
+
+        $output->writeln($text);
+    }
+
+    protected function getSecretKey()
+    {
+        return $this->getContainer()->getParameter('forestadmin.forest.secret_key');
+    }
+}
