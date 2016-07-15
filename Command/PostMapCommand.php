1578648f08c9f10583b9ce6e0d18c94d2f08965c
jean-marc <jean-marc@lg21.be>
2016-07-15 11:44:32 +0200

    added log informations on postmap

--- Command/PostMapCommand.php
+++ Command/PostMapCommand.php
@@ -7,6 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Logger\ConsoleLogger;
 use Symfony\Component\Console\Output\OutputInterface;
 use GuzzleHttp\Exception\ClientException;
 use Symfony\Component\DependencyInjection\Exception\InvalidArgumentException;
@@ -19,34 +20,35 @@ class PostMapCommand extends ContainerAwareCommand
         $this
             ->setName('forest:postmap')
             ->setDescription('Post the API map to the Forest Server')
-//            ->addArgument(
-//                'env',
-//                InputArgument::OPTIONAL,
-//                'From which environment does come the map that you want to post?'
-//            )
         ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        try {
-            //$this->getSecretKey();
-            $forest = $this->getContainer()->get('forestadmin.forest');
-            //$name = $input->getArgument('name');
+        $logger = new ConsoleLogger($output);
 
-                if($forest->postApimap()) {
-                    $text = 'Success!';
-                } else {
-                    $text = 'Failure!';
-                }
+        try {
+            $this->getSecretKey();
+            $forest = $this->getContainer()->get('forestadmin.forest');
+
+            if($forest->postApimap()) {
+                $text = 'Success!';
+            } else {
+                $logger->error('Could not post API map for an unknown reason.');
+                $text = 'Failure!';
+            }
         } catch(ClientException $exc) {
-            $text = 'Client Failure: ' . $exc->getMessage();
+            $logger->error('Client Failure: ' . $exc->getMessage());
+            $text = 'Cannot contact the client.';
         } catch(RequestException $exc) {
-            $text = 'Request Failure: ' . $exc->getMessage();
+            $logger->critical('Request Failure: ' . $exc->getMessage());
+            $text = 'Malformed request.';
         } catch(InvalidArgumentException $exc) {
+            $logger->debug('Client tried to contact us without secret key');
             $text = "Failure: ".$exc->getMessage()."\n"
             ."Configure your secret key with the key you received when you registered your app to Forest.";
         } catch(\Exception $exc) {
+            $logger->error('Unexpected exception triggered: ' . $exc->getMessage());
             $text = get_class($exc).' Failure: '.$exc->getMessage();
         }
 
